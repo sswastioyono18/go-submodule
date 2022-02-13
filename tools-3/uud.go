efc07a001c9413fc74de460486fad34f16d73aa6
Sactio Swastioyono <email.tyo@gmail.com>
2022-02-13 22:17:06 +0700

    test sub tools 3

--- tools-3/uud.go
+++ tools-3/uud.go
@@ -7,6 +7,6 @@ import (
 
 func NewGofrsUUID() string {
 	strResult, _ := uuid.NewV4()
-	fmt.Println("test hehehehehe 123 hahahaha huhuhuu")
+	fmt.Println("test tools-3 berubah")
 	return strResult.String()
 }
