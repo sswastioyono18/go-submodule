e0ce4e5cde143bb7e3185ae5455f35d7a8c127d9
Sactio Swastioyono <email.tyo@gmail.com>
2022-02-13 17:20:59 +0700

    test release tools 2

--- tools-2/uud.go
+++ tools-2/uud.go
@@ -1,8 +1,12 @@
 package tools_2
 
-import "github.com/gofrs/uuid"
+import (
+	"github.com/gofrs/uuid"
+	"github.com/rs/zerolog/log"
+)
 
 func NewGofrsUUID() string {
 	strResult, _ := uuid.NewV4()
+	log.Logger.Info().Msg("Test123")
 	return strResult.String()
 }
